6a09bc1697755f9e2651abe86cbd9ba12924f1a7
Shalom Ormsby <shalomormsby@gmail.com>
2026-01-11 07:56:50 -0800

    Fix: Build error

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -4,8 +4,13 @@ export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
   dts: {
+    resolve: true,
     compilerOptions: {
       skipLibCheck: true,
+      skipDefaultLibCheck: true,
+      noImplicitAny: false,
+      strictNullChecks: false,
+      strict: false,
     },
   },
   splitting: false,
